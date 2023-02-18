0f699dd26510409bbcb75af4e2926f0736e2fd5e
Gera <german.titoww@gmail.com>
2023-02-18 20:09:34 +0300

    homeworks  done

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -82,3 +82,118 @@
 //     current = current + 2;
 // };
 // };
+//Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int Prompt (string message)
+// {
+
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+
+// }
+
+// int number = Prompt("Enter a three-digit number > ");
+// if (number < 100 || number >= 1000)
+// {
+//     Console.WriteLine("You have not entered a three-digit number, please re-enter");
+//     return;
+// }
+// Console.WriteLine($"Entered number `{number}`");
+// int SecondRank = number / 10 % 10;
+// Console.WriteLine($" Second digit `{SecondRank}` ");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// int Prompt (string message)
+//  {
+
+//      Console.Write(message);
+//      string value = Console.ReadLine();
+//      int result = Convert.ToInt32(value);
+//      return result;
+
+//  }
+
+//  int GetThirdRank (int number)
+//  {
+//     while (number > 999)
+//     {
+//         number /= 10;
+//     }
+//     return number % 10;
+
+//  }
+//  bool ValidateNumber (int number)
+//  {
+
+//     if (number < 100)
+// {
+
+//     Console.WriteLine(" No third digit");
+//     return false;
+// }
+// return true ;
+//  }
+//  int number = Prompt("Enter the number > ");
+//  if (ValidateNumber(number))
+//  {
+//     Console.WriteLine(GetThirdRank(number));
+//  }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+  int Prompt (string message)
+  {
+
+       Console.Write(message);
+       string value = Console.ReadLine();
+       int result = Convert.ToInt32(value);
+       return result;
+
+   }
+
+bool IsWeekend(int weekDay)
+{
+     if (weekDay > 5)
+    {
+     return true;
+    } 
+    return false;
+ }
+bool ValidateWeekday (int number)
+{
+if (number > 0 && number <= 7)
+ {
+    return true;
+ }
+ Console.WriteLine("Это не день недели!");
+ return false;
+}
+int weekDay = Prompt("Введите день недели >");
+    if (ValidateWeekday(weekDay))
+{
+    Console.WriteLine("Наконец-то выходной");
+}
+else
+{
+    Console.WriteLine("Придется работать");
+}
+
+
+
